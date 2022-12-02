6c42ef86861eeb2b1ac50038b7d51ca3ff48591d
jose ismael rodriguez <jr3376@hotmail.com>
2022-12-02 19:53:49 -0300

    trabajo pratico 10

---
+++ variables2.js
@@ -0,0 +1,30 @@
+let programaciòn= "programación" + " fácil";  
+let saludo = "Les doy la bienvenida a ";
+let entretenida_R_D = " y entretenida de Revoluciòn Digital";
+let fraseCompleta = saludo + programaciòn + entretenida_R_D;
+console.log(fraseCompleta);
+
+let frase = "JavaScript " + "es un lenguaje" + " de programaciòn" + " esencial para crear" + " sitios web poderosos.";
+console.log(frase);
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+let concatenaNumeros = numeroString1 + " " + "suma" + " " + numeroString2 + numeroString3;
+console.log(concatenaNumeros);
+
+
+let nombre = prompt ("cual es tu nombre?");
+let edad = parseInt (prompt ("cual es tu edad?"));
+let info =  nombre + " tiene " + edad + " años ";
+console.warn (info);
+
+
+
+
+
+
+
+
+
+
